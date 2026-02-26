565366d35d798f7bd95c798fc97906ede5b15389
Juan Campos <juan@learnbrite.com>
2026-02-26 14:45:24 +0100

    add option to set iris occlusion values from constructor

--- src/renderer/GaussianSplatRenderer.js
+++ src/renderer/GaussianSplatRenderer.js
@@ -295,7 +295,12 @@ export class GaussianSplatRenderer {
             logger.debug('Checking for iris_occlusion.json');
             let irisOcclusionConfig = null;
             try {
-                irisOcclusionConfig = await renderer._loadJsonFromZip(fileName + '/iris_occlusion.json');
+                
+                if (options.irisOcclusionConfig) {
+                    irisOcclusionConfig = options.irisOcclusionConfig; // Override with options if provided
+                } else {
+                    irisOcclusionConfig = await renderer._loadJsonFromZip(fileName + '/iris_occlusion.json');
+                }
                 if (irisOcclusionConfig) {
                     logger.info('Iris occlusion configuration loaded', {
                         rightIrisRanges: irisOcclusionConfig.right_iris?.length ?? 0,
